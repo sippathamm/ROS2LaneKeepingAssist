572acd1b92622f5f51cc941ee5882c08d401bffd
sippathamm <sippathamm@gmail.com>
2024-08-29 12:41:30 +0700

    Display feedback_speed on steering_debug image

--- lane_keeping_assist/lane_keeping_assist/steering_predictor.py
+++ lane_keeping_assist/lane_keeping_assist/steering_predictor.py
@@ -5,7 +5,7 @@ Date: 06.08.2024
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16, Bool
+from std_msgs.msg import Int16, Float32, Bool
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ament_index_python.packages import get_package_share_directory
@@ -23,6 +23,7 @@ class SteeringPredictor(Node):
 
         self.declare_parameter('cmd_steering_topic', 'cmd_servo')
         self.declare_parameter('cmd_speed_topic', 'cmd_throttle')
+        self.declare_parameter('feedback_speed_topic', 'vel_rpt')
         self.declare_parameter('image_topic', 'image_raw')
         self.declare_parameter('max_speed', 100)
         self.declare_parameter('min_speed', 80)
@@ -34,6 +35,7 @@ class SteeringPredictor(Node):
 
         self.CMD_STEERING_TOPIC = self.get_parameter('cmd_steering_topic').get_parameter_value().string_value
         self.CMD_SPEED_TOPIC = self.get_parameter('cmd_speed_topic').get_parameter_value().string_value
+        self.FEEDBACK_SPEED_TOPIC = self.get_parameter('feedback_speed_topic').get_parameter_value().string_value
         self.IMAGE_TOPIC = self.get_parameter('image_topic').get_parameter_value().string_value
         self.MAX_SPEED = self.get_parameter('max_speed').get_parameter_value().integer_value
         self.MIN_SPEED = self.get_parameter('min_speed').get_parameter_value().integer_value
@@ -47,12 +49,14 @@ class SteeringPredictor(Node):
         self.cmd_speed_publisher = self.create_publisher(Int16, self.CMD_SPEED_TOPIC, 10)
         self.steering_debug_publisher = self.create_publisher(Image, 'steering_debug', 10)
         self.image_subscriber = self.create_subscription(Image, self.IMAGE_TOPIC, self.image_callback, 10)
+        self.feedback_speed_subscriber = self.create_subscription(Float32, self.FEEDBACK_SPEED_TOPIC, self.feedback_speed_callback, 10)
 
         self.bridge = CvBridge()
         self.steering_wheel_image = cv2.imread(os.path.join(get_package_share_directory('lane_keeping_assist'),
                                                             'share', 'images', 'steering-wheel-14-256.png'),
                                                cv2.IMREAD_UNCHANGED)
         self.steering_wheel_image = cv2.resize(self.steering_wheel_image, (100, 100))
+        self.feedback_speed_ms = 0
 
         self.session = onnxruntime.InferenceSession(
             os.path.join(get_package_share_directory('lane_keeping_assist'),
@@ -61,6 +65,9 @@ class SteeringPredictor(Node):
         )
 
         self.get_logger().info('> Initialized without any errors. Waiting for image...')
+
+    def feedback_speed_callback(self, msg):
+        self.feedback_speed_ms = msg.data
 
     def image_callback(self, msg):
         frame = self.bridge.imgmsg_to_cv2(msg, 'rgb8')
@@ -85,12 +92,12 @@ class SteeringPredictor(Node):
                                       (self.TURN_RIGHT_CMD_STEERING - self.TURN_LEFT_CMD_STEERING) /
                                       (self.TURN_RIGHT_STEERING_ANGLE_RAD - self.TURN_LEFT_STEERING_ANGLE_RAD) +
                                       self.TURN_LEFT_CMD_STEERING)
-        self.cmd_steering_publisher.publish(cmd_steering_angle)
+        # self.cmd_steering_publisher.publish(cmd_steering_angle)
 
         cmd_speed = Int16()
         k = 1 if normalized_steering_angle < 0 else -1
         cmd_speed.data = int(k * (self.MAX_SPEED - self.MIN_SPEED) * normalized_steering_angle / 1.0 + self.MAX_SPEED)
-        self.cmd_speed_publisher.publish(cmd_speed)
+        # self.cmd_speed_publisher.publish(cmd_speed)
 
         self.get_logger().info('\n'
                                '       > Steering CMD: %d\n'
@@ -108,7 +115,9 @@ class SteeringPredictor(Node):
         rotation_matrix = cv2.getRotationMatrix2D(center, np.rad2deg(normalized_steering_angle) * 0.5, 1.0)
         rotated_steering_wheel = cv2.warpAffine(self.steering_wheel_image, rotation_matrix, (w, h),
                                                 flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_TRANSPARENT)
-        cv2.putText(background, 'Steering angle: ' + str(normalized_steering_angle), (30, 150),
+        cv2.putText(background, 'Normalized steering angle: ' + str(normalized_steering_angle), (30, 150),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.putText(background, 'Current speed [m/s]: ' + str('{:.3f}'.format(self.feedback_speed_ms)), (30, 170),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
         add_transparent_image(background, rotated_steering_wheel, 30, 30)
 
