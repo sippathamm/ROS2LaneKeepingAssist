0473bcc0a5465d84e649aa1eb0cf0dd148b62581
sippathamm <sippathamm@gmail.com>
2024-09-10 13:34:39 +0700

    Implement handling for missing lanes

--- lane_keeping_assist/lane_keeping_assist/core.py
+++ lane_keeping_assist/lane_keeping_assist/core.py
@@ -10,6 +10,8 @@ from std_msgs.msg import Int16, Float32
 from custom_msgs.msg import Coefficients
 from ament_index_python.packages import get_package_share_directory
 from .utils import colors
+import numpy as np
+import collections
 
 
 class CoreNode(Node):
@@ -36,7 +38,7 @@ class CoreNode(Node):
             'left': self.create_subscription(Coefficients, 'lane_detector/left_lane/coefficients',
                                              self.__left_lane_coeff_callback, 10),
             'right': self.create_subscription(Coefficients, 'lane_detector/right_lane/coefficients',
-                                             self.__right_lane_coeff_callback, 10)
+                                              self.__right_lane_coeff_callback, 10)
         }
 
         # Messages
@@ -47,6 +49,9 @@ class CoreNode(Node):
             'left': Coefficients(),
             'right': Coefficients(),
         }
+
+        self.state = 'straight'
+        self.recent_states = collections.deque([], maxlen=10)
 
         self.get_logger().info(
             f'{colors.OKGREEN}> OK.{colors.ENDC}'
@@ -73,26 +78,52 @@ class CoreNode(Node):
     def __timer_callback(self) -> None:
         steering_angle = self.steering_angle.data * self.GAIN
 
+        if steering_angle >= 0.25:
+            self.state = 'turn_left'
+        elif steering_angle <= -0.25:
+            self.state = 'turn_right'
+        else:
+            self.state = 'straight'
+
+        # self.recent_states.append(self.state)
+        #
+        # # self.get_logger().info(f'deque: {self.recent_states}')
+        # uniques, counts = np.unique(self.recent_states, return_counts=True)
+        # self.get_logger().info(f'unique: {uniques}, count: {counts}')
+        #
+        # most_count = np.argmax(counts)
+        # self.get_logger().info(f'most count: {uniques[most_count]}')
+
+        self.get_logger().info(self.state)
+
         # If there are no left and right lanes, stop the car.
-        if self.lane_coeffs['left'].data == self.ZERO_COEFFS.data and \
-           self.lane_coeffs['right'].data == self.ZERO_COEFFS.data:
-            cmd_steering = 0
-            cmd_speed = 0
+        if (self.lane_coeffs['left'].data == self.ZERO_COEFFS.data and
+            self.lane_coeffs['right'].data == self.ZERO_COEFFS.data) or \
+                (self.lane_coeffs['left'] == Coefficients() and
+                 self.lane_coeffs['right'] == Coefficients()):
+            steering_angle = 0
 
             self.get_logger().info('[WARNING] Left and right lane are not detected. Stop the car.')
-        else:
-            cmd_steering = self.steering_angle_to_cmd_steering(steering_angle)
-            cmd_speed = self.steering_angle_to_cmd_speed(steering_angle)
+        elif self.lane_coeffs['left'].data == self.ZERO_COEFFS.data and \
+                self.state == 'turn_left':
+            steering_angle = 0.5
+        elif self.lane_coeffs['right'].data == self.ZERO_COEFFS.data and \
+                self.state == 'turn_right':
+            steering_angle = -0.5
 
-            self.get_logger().info('\n'
-                                   '       > Predicted steering angle: %f\n'
-                                   '       > CMD steering: %d\n'
-                                   '       > CMD speed: %d\n' %
-                                   (steering_angle,
-                                    cmd_steering,
-                                    cmd_speed
-                                    )
-                                   )
+        cmd_steering = self.steering_angle_to_cmd_steering(steering_angle)
+        cmd_speed = self.steering_angle_to_cmd_speed(steering_angle)
+        # cmd_speed = 0
+
+        self.get_logger().info('\n'
+                               '       > Predicted steering angle: %f\n'
+                               '       > CMD steering: %d\n'
+                               '       > CMD speed: %d\n' %
+                               (steering_angle,
+                                cmd_steering,
+                                cmd_speed
+                                )
+                               )
 
         self.publish_cmd_steering(cmd_steering)
         self.publish_cmd_speed(cmd_speed)
