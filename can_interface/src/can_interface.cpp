7598ba7ae3a0e614c3b2ac2c903658b99f7a6643
sippathamm <sippathamm@gmail.com>
2024-09-20 12:52:53 +0700

    Implement CAN transmit and receive

--- can_interface/src/can_interface.cpp
+++ can_interface/src/can_interface.cpp
@@ -4,57 +4,61 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int16.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 #include "can_interface/controlcan.h"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class CANInterface : public rclcpp::Node
 {
   public:
     CANInterface () : Node("can_interface")
     {
-      this->Timer_ = this->create_wall_timer(500ms, std::bind(&CANInterface::TimerCallback, this));
+      // Timers
+      this->CANTransmitTimer_ = this->create_wall_timer(0.033ms, std::bind(&CANInterface::CANTransmitTimerCallback, this));
+      this->CANReceiveTimer_ = this->create_wall_timer(20ms, std::bind(&CANInterface::CANReceiveTimerCallback, this));
+
+      // Publishers
+      this->FBKSteeringAnglePublisher_ = this->create_publisher<std_msgs::msg::Float32>("feedback/steering_angle", 10);
+      this->FBKSpeedPublisher = this->create_publisher<std_msgs::msg::Float32>("feedback/speed", 10);
+
+      // Subscribers
+      this->CMDSteeringSubscriber_ = this->create_subscription<std_msgs::msg::Float32>("cmd_steering",10,
+        std::bind(&CANInterface::CMDSteeringCallback, this, _1));
+      this->CMDSpeedSubscriber_ = this->create_subscription<std_msgs::msg::Float32>("cmd_speed",10,
+        std::bind(&CANInterface::CMDSpeedCallback, this, _1));
 
       this->CANInitialize();
+      this->CANTransmitSetup();
+      this->CANReceiveSetup();
     }
 
     ~CANInterface () override
     {
       VCI_ResetCAN(VCI_USBCAN1, 0, 0);
       VCI_CloseDevice(VCI_USBCAN1,0);
+      
+      delete[] this->TxBuffer_;
+      delete[] this->RxBuffer_;
     }
 
   private:
-    rclcpp::TimerBase::SharedPtr Timer_;
-
-    void TimerCallback ()
-    {
-      VCI_CAN_OBJ CANSend[1];
-      CANSend[0].ID = 0x201;
-      CANSend[0].SendType = 0;
-      CANSend[0].RemoteFlag = 0;
-      CANSend[0].ExternFlag = 0;
-      CANSend[0].DataLen = 8;
-      CANSend[0].Data[0] = 123;
-      CANSend[0].Data[1] = 0;
-      CANSend[0].Data[2] = 0;
-      CANSend[0].Data[3] = 0;
-      CANSend[0].Data[4] = 0;
-      CANSend[0].Data[5] = 0;
-      CANSend[0].Data[6] = 0;
-      CANSend[0].Data[7] = 0;
-
-      if (VCI_Transmit(VCI_USBCAN1, 0, 0, CANSend, 1))
-      {
-        RCLCPP_INFO(this->get_logger(), "> Sent OK.");
-      }
-      else
-      {
-        RCLCPP_INFO(this->get_logger(), "> Sent failed.");
-      }
-    }
+    rclcpp::TimerBase::SharedPtr CANTransmitTimer_;
+    rclcpp::TimerBase::SharedPtr CANReceiveTimer_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr FBKSteeringAnglePublisher_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr FBKSpeedPublisher;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr CMDSteeringSubscriber_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr CMDSpeedSubscriber_;
+    std_msgs::msg::Float32 CMDSteering_;
+    std_msgs::msg::Float32 CMDSpeed_;
+    float FeedbackSteeringAngle_ = 0.0;
+    float FeedbackSpeed_ = 0.0;
+    unsigned char *TxBuffer_ = new unsigned char [8];
+    unsigned char *RxBuffer_ = new unsigned char [8];
+    VCI_CAN_OBJ CANTransmit_[1] {};
+    VCI_CAN_OBJ CANReceive_[1] {};
 
     void CANInitialize () const
     {
@@ -116,6 +120,125 @@ class CANInterface : public rclcpp::Node
         RCLCPP_INFO(this->get_logger(), "> Failed to start CAN1.");
         VCI_CloseDevice(VCI_USBCAN1,0);
       }
+    }
+
+    void CANTransmitSetup ()
+    {
+      this->CANTransmit_[0].ID = 0x202;
+      this->CANTransmit_[0].SendType = 0;
+      this->CANTransmit_[0].RemoteFlag = 0;
+      this->CANTransmit_[0].ExternFlag = 0;
+      this->CANTransmit_[0].DataLen = 8;
+    }
+
+    void CANReceiveSetup ()
+    {
+      this->CANReceive_[0].ID = 0x182;
+      this->CANReceive_[0].SendType = 1;
+      this->CANReceive_[0].RemoteFlag = 0;
+      this->CANReceive_[0].ExternFlag = 0;
+      this->CANReceive_[0].DataLen = 8;
+    }
+
+    void CANTransmitTimerCallback ()
+    {
+      float CMDSteering = this->CMDSteering_.data;
+      float CMDSpeed = this->CMDSpeed_.data;
+      const auto *CMDSteeringPointer = reinterpret_cast<unsigned char*>(&CMDSteering);
+      const auto *CMDSpeedPointer = reinterpret_cast<unsigned char*>(&CMDSpeed);
+      this->TxBuffer_[0] = CMDSpeedPointer[0];
+      this->TxBuffer_[1] = CMDSpeedPointer[1];
+      this->TxBuffer_[2] = CMDSpeedPointer[2];
+      this->TxBuffer_[3] = CMDSpeedPointer[3];
+      this->TxBuffer_[4] = CMDSteeringPointer[0];
+      this->TxBuffer_[5] = CMDSteeringPointer[1];
+      this->TxBuffer_[6] = CMDSteeringPointer[2];
+      this->TxBuffer_[7] = CMDSteeringPointer[3];
+
+      for (int i = 0; i < 8; i++)
+      {
+        this->CANTransmit_[0].Data[i] = this->TxBuffer_[i];
+      }
+
+      if (VCI_Transmit(VCI_USBCAN1, 0, 0, this->CANTransmit_, 1))
+      {
+        // RCLCPP_INFO(this->get_logger(), "\n"
+        //                                 "       > CMD Steering: %f\n"
+        //                                 "       > CMD Speed: %f",
+        //                                 CMDSteering,
+        //                                 CMDSpeed);
+        // RCLCPP_INFO(this->get_logger(), "> Sent OK.");
+      }
+      else
+      {
+        RCLCPP_INFO(this->get_logger(), "> Sent failed.");
+      }
+    }
+
+    void CANReceiveTimerCallback ()
+    {
+      auto Frame = VCI_Receive(VCI_USBCAN1, 0, 0, this->CANReceive_, 1, 5);
+
+      if (Frame == 1)
+      {
+        ClearBuffer(this->RxBuffer_);
+
+        // RCLCPP_INFO(this->get_logger(), "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
+        //   this->CANReceive_[0].Data[0],
+        //   this->CANReceive_[0].Data[1],
+        //   this->CANReceive_[0].Data[2],
+        //   this->CANReceive_[0].Data[3],
+        //   this->CANReceive_[0].Data[4],
+        //   this->CANReceive_[0].Data[5],
+        //   this->CANReceive_[0].Data[6],
+        //   this->CANReceive_[0].Data[7]
+        //   );
+
+        this->RxBuffer_[0] = this->CANReceive_[0].Data[0];
+        this->RxBuffer_[1] = this->CANReceive_[0].Data[1];
+        this->RxBuffer_[2] = this->CANReceive_[0].Data[2];
+        this->RxBuffer_[3] = this->CANReceive_[0].Data[3];
+        this->RxBuffer_[4] = this->CANReceive_[0].Data[4];
+        this->RxBuffer_[5] = this->CANReceive_[0].Data[5];
+        this->RxBuffer_[6] = this->CANReceive_[0].Data[6];
+        this->RxBuffer_[7] = this->CANReceive_[0].Data[7];
+
+        // this->FeedbackSteeringAngle_ = *(float *) &this->RxBuffer_[4];
+        // this->FeedbackSpeed_ = *(float *) &this->RxBuffer_[0];
+
+        this->FeedbackSteeringAngle_ = *reinterpret_cast<float*>(&this->RxBuffer_[4]);
+        this->FeedbackSpeed_ = *reinterpret_cast<float*>(&this->RxBuffer_[0]);
+
+        RCLCPP_INFO(this->get_logger(), "\n"
+                                        "       > Feedback steering angle [deg]: %f\n"
+                                        "       > Feedback speed [m/s]: %f",
+                                        this->FeedbackSteeringAngle_,
+                                        this->FeedbackSpeed_);
+
+        std_msgs::msg::Float32 FeedbackSteeringAngleMsg;
+        FeedbackSteeringAngleMsg.data = this->FeedbackSteeringAngle_;
+
+        std_msgs::msg::Float32 FeedbackSpeedMsg;
+        FeedbackSpeedMsg.data = this->FeedbackSpeed_;
+
+        this->FBKSteeringAnglePublisher_->publish(FeedbackSteeringAngleMsg);
+        this->FBKSpeedPublisher->publish(FeedbackSpeedMsg);
+      }
+    }
+
+    static void ClearBuffer (unsigned char *Buffer)
+    {
+      for (int i = 0; i < 8; i++) { Buffer[i] = 0; }
+    }
+
+    void CMDSteeringCallback (const std_msgs::msg::Float32 &Message)
+    {
+      this->CMDSteering_ = Message;
+    }
+
+    void CMDSpeedCallback (const std_msgs::msg::Float32 &Message)
+    {
+      this->CMDSpeed_ = Message;
     }
 };
 
